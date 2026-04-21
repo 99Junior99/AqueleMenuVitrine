08179570e085c103577dfda6aaa2f31abff1d464
Junior <joseinadilsonjunior@gmail.com>
2026-04-21 20:10:18 -0300

    tentando usar gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // As rotas
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/AqueleMenuVitrine",
     element: <Home />,
 
   },
